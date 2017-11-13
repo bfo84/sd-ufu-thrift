857584fa6ee34e6e09158069c12b5a4fced16fb3
Ramon <ramonvlopes@hotmail.com>
2017-11-12 22:09:16 -0200

    start servers configure

--- src/main/java/br/ufu/miguelpereira/view/Server.java
+++ src/main/java/br/ufu/miguelpereira/view/Server.java
@@ -37,12 +37,6 @@ public class Server {
             Operations.Processor processor = new Operations.Processor(handler);
             TServerTransport serverTransport = new TServerSocket(port);
             TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
-            //Criar concorrencia com apenas um thread
-            //TNonblockingServerSocket serverTransport = new TNonblockingServerSocket(9080);
-            //THsHaServer.Args argus = new THsHaServer.Args(serverTransport);
-            //argus.processor(processor);
-            //argus.transportFactory(new TFramedTransport.Factory());
-            //TServer server = new THsHaServer(argus);
             System.out.println("Servidor Inicializado...");
             server.serve();
         } catch (Exception x){
